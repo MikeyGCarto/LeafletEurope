947bb60484b49769b9aa2bc34b9e01aad4e98534
MikeyGCarto <117098956+MikeyGCarto@users.noreply.github.com>
2024-03-06 03:24:49 -0600

    Update main.js

--- Unit2/js/main.js
+++ Unit2/js/main.js
@@ -2,9 +2,11 @@ var map = L.map('map').setView([47.5000, 10.0000], 5);
 var data; // Define data in a broader scope
 
 //Add Tile Layer
-var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
+var Stadia_StamenTonerLite = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
 });
 
 // Define a custom control for the slider and buttons
