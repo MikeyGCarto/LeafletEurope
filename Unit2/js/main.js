9ab934b94c92d3e172e9769dd0f6a1827185ec4d
MikeyGCarto <117098956+MikeyGCarto@users.noreply.github.com>
2024-03-06 04:22:47 -0600

    Update main.js

--- Unit2/js/main.js
+++ Unit2/js/main.js
@@ -1,10 +1,12 @@
 var map = L.map('map').setView([47.5000, 10.0000], 5);
 var data; // Define data in a broader scope
 
-// Add Tile Layer
-var OpenStreetMap_DE = L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// Add tile layer
+var Stadia_StamenTonerLite = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}', {
+    minZoom: 0,
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    ext: 'png'
 }).addTo(map);
 
 // Define a custom control for the slider and buttons
